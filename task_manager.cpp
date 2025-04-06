fe3d9ee5cd67a5afe0981ad87a5fabb13e7894e1
Lam Doan <142973828+lam-doan@users.noreply.github.com>
2025-04-06 00:56:37 +0000

    function included

--- task_manager.cpp
+++ task_manager.cpp
@@ -1,95 +1,52 @@
 #include <iostream>
-#include <map>
 #include <string>
-#include <fstream>
-#include <sstream>
-#include <unordered_map>
 #include <vector>
+#include <iomanip> 
 
+#include "task_manager.hpp"
 
-using namespace std;
-
-int main(){
-
-    // task name, pair(type, detail)
-    unordered_map<string, vector<pair<string, string>>> tasks;
-
-    cout << "Menu Option: \n" << "Create\n" << "Edit\n" << "Delete\n" << "View\n\n";
-    cout << "Task List: " << endl;
-
-    fstream file("taskDataFile.txt", ios::in|ios::out|ios::app);          // It will open the existing file or create one if doesn't exist
-
-    if(!file.is_open()){
-        cout << "Faile to open the file\n";
-        return -1;
-    }
-
-    // To print out the list of tasks
-    string line;
-    while(getline(file, line)){
-        
-        // Print out all the task name
-        cout << line << endl;
-
-        // Use the line (task name) to open another text file to load the task detail
-        string taskName = line + ".txt";
-        fstream taskFile(taskName, ios::in|ios::out|ios::app);
-    
-
-        string detail;
-        while(getline(taskFile, detail)){
-            // Separate the task information and type using colon
-            size_t colon = detail.find(':');
-
-            if(colon != string::npos){
-                string beforeColon = detail.substr(0, colon);
-                string afterColon = (colon + 1 < detail.size())?detail.substr(colon + 1) : "";
-
-                // Push all the information into the vector, line = task name
-                tasks[line].push_back(make_pair(beforeColon, afterColon));
-            }
-        }
-
-        taskFile.close();
-
-    }
-
-    file.close();                   // Close the file that contains all the tasks name
-
-
-    /* 
-    **Print function for testing**
-    for(const auto& pair: tasks){
-        cout << "task name: " << pair.first << endl;
-        for(const auto& p: pair.second){
-            // due date, task description, location, categories
-            cout << "type: " << p.first << ", detal: " << p.second << endl;
+bool TaskManager::AddTask(Task& new_task) {
+    bool found = true;
+    for (auto& task : manager_) {
+        if (task.Description() == new_task.Description() &&
+            task.Location()    == new_task.Location()    &&
+            task.DueDate()     == new_task.DueDate()     &&
+            task.Category()    == new_task.Category()) {
+            return false;
         }
     }
-    */
+    manager_.push_back(new_task);
+    return found;
+}
 
-    string response; 
-    cout << "\n================================\n";
-    cout << "What would you like to do today?\n";
-    cin >> response;
-
-    if(response == "create"){
-        // Call create function
-
-    }   
-    else if(response == "edit"){
-        // Call edit function
-
-    } 
-    else if(response == "delete"){
-        // Call delete function
-
+bool TaskManager::DeleteTask(Task& task) {
+    bool found = false;
+    for (auto it = manager_.begin(); it != manager_.end(); ++it) {
+        if (it->Description() == task.Description() &&
+            it->Location()    == task.Location()    &&
+            it->DueDate()     == task.DueDate()     &&
+            it->Category()    == task.Category()) {
+            manager_.erase(it);
+            return true;
+        }
     }
-    else if(response == "view"){
-        // Call view function or print the map's first again
+    return found;
+}
 
+void TaskManager::DisplayTask() const {
+    std::cout << std::left;
+    std::cout << std::setw(5) << "ID"
+              << std::setw(25) << "Description"
+              << std::setw(20) << "Location"
+              << std::setw(20) << "Due date"
+              << std::setw(15) << "Category" << std::endl;
+    std::cout << std::string(78, '-') << std::endl;
+    for (int i = 0; i < manager_.size(); ++i) {
+        std::cout << std::setw(5) << i
+                  << std::setw(25) << manager_[i].Description()
+                  << std::setw(20) << manager_[i].Location()
+                  << std::setw(20) << manager_[i].DueDate()
+                  << std::setw(15) << manager_[i].Category()
+                  << std::endl;
     }
-
-        
-    return 0;
 }
